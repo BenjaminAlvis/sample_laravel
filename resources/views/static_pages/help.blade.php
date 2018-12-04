a4f66b99e44eb229aa2bd7fd2409701ecea88465
benjamin <wangbin042@gmail.com>
2018-12-04 14:19:55 +0000

    Finish static pages

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Sample App</title>
-</head>
-<body>
-  <h1>帮助页</h1>
-</body>
-</html>
+@extends('layouts/default')
+@section('title','帮助')
+
+
+@section('content')
+	<h1>help page</h1>
+
+@stop
