f9a0ecae1daccfef880c9827e86b095ec1455ee4
benjamin <wangbin042@gmail.com>
2018-12-05 01:41:18 +0000

    done with show user info

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', $user->name)
+@section('content')
+<h1>{{ $user->name }} - {{ $user->email }}</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Route::get('/about','StaticPagesController@about')->name('about');
 
 
 Route::get('signup', 'UsersController@create')->name('signup');
-
+Route::resource('users', 'UsersController');
 
